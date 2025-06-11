95bf0984b38a0287fa8d915cae1cb64d431adfe0
Evgeniy Shpilco <callelia@yandex.ru>
2025-06-11 21:46:56 +0600

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,14 @@ function Footer() {
         <div className="footer-copyright">
             <div className="container">
                 © {new Date().getFullYear()} Copyright Text
-                <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+                <a
+                    className="grey-text text-lighten-4 right"
+                    href="https://github.com/Shpilko/react-shop"
+                    target='_blank'
+                    rel='noreferrer'
+                >
+                    Repo
+                </a>
             </div>
         </div>
     </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
     return <nav className="blue darken-3">
         <div className="nav-wrapper">
-            <a href="!#" className="brand-logo">React Shop</a>
+            <a href="/" className="brand-logo">React Shop</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="!#">Repo</a></li>
+                <li><a href="https://github.com/Shpilko/react-shop" target='_blank' rel='noreferrer'>Repo</a></li>
             </ul>
         </div>
     </nav>
